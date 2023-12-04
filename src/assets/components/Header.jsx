794ae1638e99fda1702e8764a710f7fba08a78b6
RaulTindogan <tindoganraul@gmail.com>
2023-12-04 18:03:02 +0800

    update

--- src/assets/components/Header.jsx
+++ src/assets/components/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from "react-router-dom"
 function Header() {
   return (
     <div>
-        <Link to='/'>Home</Link>
-        <Link to='/discover'>Discover</Link>
-        <Link to='/signin'>Signin</Link>
-        <Link to='/login'>Login</Link>
+        <Link to='/#/'>Home</Link>
+        <Link to='/#/discover'>Discover</Link>
+        <Link to='/#/signin'>Signin</Link>
+        <Link to='/#/login'>Login</Link>
     </div>
   )
 }
