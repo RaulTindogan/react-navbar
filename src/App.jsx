d02102dd96d3f60a29b68a83aa6f526d1e118aae
RaulTindogan <tindoganraul@gmail.com>
2023-12-04 17:25:11 +0800

    Update

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
         <h1>Hello World</h1>
         <Header />
       <Routes>
-        <Route path="/" exact element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/discover" element={<Discover />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/login" element={<Login />} />
