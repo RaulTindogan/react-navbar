180b73b48c3f78d694bd78179b540fa81c28fb23
RaulTindogan <tindoganraul@gmail.com>
2023-12-04 16:09:58 +0800

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import Header from './assets/components/Header'
+import Home from './assets/components/Home'
+import Discover from './assets/components/Discover'
+import SignIn from './assets/components/Signin'
+import Login from './assets/components/Login'
+
+
+function App() {
+
+  return (
+    <Router>
+      <div>
+        <h1>Hello World</h1>
+        <Header />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/discover" element={<Discover />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+        
+      </div>
+    </Router>
+  )
+}
+
+export default App
---
+++ src/assets/components/Header.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom"
+
+function Header() {
+  return (
+    <div>
+        <Link to='/'>Home</Link>
+        <Link to='/discover'>Discover</Link>
+        <Link to='/signin'>Signin</Link>
+        <Link to='/login'>Login</Link>
+    </div>
+  )
+}
+
+export default Header
