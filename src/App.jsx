f5c5ed607e2469618045e71e72fb674c9bd6e460
RaulTindogan <tindoganraul@gmail.com>
2023-12-04 16:54:23 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './App.css'
-import { BrowserRouter as Router,Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Header from './assets/components/Header'
 import Home from './assets/components/Home'
 import Discover from './assets/components/Discover'
